b83ff9ef918792892a7bfb1fc9df0a1333de6e8e
Erik Petersen <contacterikpetersen@gmail.com>
2016-06-01 17:40:31 -0400

    Well, still cannot get the click on one to close another but added functionality to the inner ul> clear asdljasdflasdf adsf asdf sf sadf sf asf asf asd asdk

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,50 @@
 /* Erik Petersen Week 3 Day 2: The Revenge of Nested-Accordian */
 
 var $btn = $( ".outer-ul" );
+var $innerBtn = $( ".inner-ul" );
 
 $btn.on("click", ".outer-heading", function() {
-  console.log("Button clicked");
-  $(this).next().show();
-});
+  console.log("Hello");
+  if( $(this).next().is( ":hidden" ) ) {
+    $(this).next().slideDown( 800, function() {} );
+    $(this).children( ".hide-down-arrow" ).show();
+    $(this).children( ".fa-arrow-right" ).hide();
+
+    $innerBtn.on("click", "h4", function() {
+      console.log( "yaaayyy" );
+      $(this).next().slideDown( 800, function() {} );
+    });
+
+    // if any of the other siblings that are not this are visible, hide them
+  //   for( var index = 0; index < $btn.length; index++ ) {
+  //     if( $btn.next().index() !== $(this).next().index() || $btn.next().index().is(":visible")) {
+  //       console.log("Success!!!!!!!!!!");
+  //       console.log($(this).index());
+  //       $btn.next().index().hide();
+  //     }
+  //   }
+
+  } // end if
 
 
+    else {
+      $(this).next().slideUp( 800, function() {});
+      $(this).children( ".hide-down-arrow" ).hide();
+      $(this).children( ".fa-arrow-right" ).show();
+    }
+}); // end click()
+
+
+
+
+
+// when button is clicked, if it is closed, I want it to open. If it is open, I want it to close.
+// if any other boxes are open, I want them to close.
+
+
+// $( "#book" ).slideDown( "slow", function() {
+//     // Animation complete.
+//   });
 // for( var index = 0; index < btn.length; index++ ) {
 //   $btn[index].click( function() {
 //     console.log("Button clicked");
